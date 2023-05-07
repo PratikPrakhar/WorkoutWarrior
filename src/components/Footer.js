aa28031560844e30768d9a0926a37d1179bdeffe
PratikPrakhar <pratikprakhar54@gmail.com>
2023-05-07 23:16:57 +0530

    1st commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+
+const Footer = () => (
+  <Box mt="80px" >
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '150px', height: '200px' }} />
+    </Stack><br></br>
+  </Box>
+);
+
+export default Footer;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'f7962ed569msha36c91c63ab2195p1467a1jsn4a11dc280bec',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
